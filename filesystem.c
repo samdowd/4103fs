008a07873120c004fb59e9fd80c236652dd437b9
Ben Graham <graham.ben61@gmail.com>
2017-11-29 00:06:49 -0600

    lol

--- filesystem.c
+++ filesystem.c
@@ -12,21 +12,27 @@
 // file system error code set (set by each file system function).
 FSError fserror;
 
+// typedef struct DirBlock {
+// 	char buf[504];
+// 	int mode;
+// 	int inode;
+// } DirBlock;
+
 File lookup_file(char *name) {
-  char block[SOFTWARE_DISK_BLOCK_SIZE];
+	char block[SOFTWARE_DISK_BLOCK_SIZE];
 
-  read_sd_block(block, 257);
+	read_sd_block(block, 257);
 
-  File file;
-  printf("returning empty file from lookup: %s\n\n\n", block);
-  return file;
+	File file;
+	// printf("returning empty file from lookup: %s\n\n\n", block);
+	return file;
 }
 
 // open existing file with pathname 'name' and access mode 'mode'.  Current file
 // position is set at byte 0.  Returns NULL on error. Always sets 'fserror' global.
 File open_file(char *name, FileMode mode) {
-  lookup_file(name);
-  printf("lookup done for %s\n\n\n", name);
+	lookup_file(name);
+	// printf("lookup done for %s\n\n\n", name);
 	// check if file is closed - throw error
 	// change inode bit to open
 }
@@ -34,28 +40,88 @@ File open_file(char *name, FileMode mode) {
 // create and open new file with pathname 'name' and access mode 'mode'.  Current file
 // position is set at byte 0.  Returns NULL on error. Always sets 'fserror' global.
 File create_file(char *name, FileMode mode){
-  File file;
 
-  // check dir bit vector for 0 - update it to 1
-  int dir_bit_vector[SOFTWARE_DISK_BLOCK_SIZE];
-  read_sd_block(dir_bit_vector, 257);
-  // find first zero in dir_bit_vector, let's say it was at 9
-  file->dir_block_index = 9;
-  dir_bit_vector[9] = 1;
-  write_sd_block(dir_bit_vector, 257);
+	File file;
 
-  // check inode bit vector for 0 - update it to 1
-  int inode_bit_vector[SOFTWARE_DISK_BLOCK_SIZE];
-  read_sd_block(inode_bit_vector, 258);
-  // find first zero in inode bit vector, let's say 7
-  file->inode_block_index = 7;
-  inode_bit_vector[7] = 1;
-  write_sd_block(inode_bit_vector, 258);  
+	// check dir bit vector for 0 - update it to 1
+	int dir_bit_vector[SOFTWARE_DISK_BLOCK_SIZE];
+	read_sd_block(dir_bit_vector, 257);
+
+	int first_dir_zero = get_empty_block_index(dir_bit_vector);
+	file->dir_block_index = first_dir_zero;
+	set_bit(dir_bit_vector, first_dir_zero);
+
+	write_sd_block(dir_bit_vector, 257);
+
+	// check inode bit vector for 0 - update it to 1
+	int inode_bit_vector[SOFTWARE_DISK_BLOCK_SIZE];
+	read_sd_block(inode_bit_vector, 258);
+
+	int first_inode_zero = get_empty_block_index(inode_bit_vector);
+	file->inode_block_index = first_inode_zero;
+	set_bit(inode_bit_vector, first_inode_zero);
+
+	write_sd_block(inode_bit_vector, 258);
+
+	// Write 512 byte buffer to the first empty directory block
+	// dir_buf = name [507 bytes], mode [1 byte], block index [1 byte]
+	char dir_buf[SOFTWARE_DISK_BLOCK_SIZE];
+
+	memcpy(dir_buf, name, sizeof(name)-1);
+	for(int i = sizeof(name); i < 508; i++){
+		dir_buf[i] = ' ';
+	}
+
+	dir_buf[508] = (mode == READ_ONLY) ? 'r' : 'w';
+
+
+	//--------------Monstrosities ahead ----------------------
+
+	char index[4];
+	if(4 - strlen(file->dir_block_index+'0') == 3)
+		"000" + file->dir_block_index+'0'
+
+	dir_buf[509] = file->dir_block_index+'0';
 
 	// go to empty dir block - store name and empty inode block index
+	write_sd_block(dir_buf, file->dir_block_index);
 
-	// call open_file()
-  open_file(name, mode);
+
+	// for (int j=0; j < 508; j++) {
+ //    printf("%c", dir_buf[j]);
+ //  }
+
+
+
+
+	char buf[SOFTWARE_DISK_BLOCK_SIZE];
+  read_sd_block(buf, file->dir_block_index);
+
+  char namey[508];
+  memcpy(namey, buf, 508);
+
+  char m = buf[508];
+  
+  char ind[4];
+  // memcpy(ind, &buf[509], 4*sizeof(*buf));
+
+  for(int x=509; x<=512; x++){
+  	ind[509-x]=buf[x];
+  }
+
+  int indexy;
+
+	sscanf(ind, "%d", &indexy);
+
+  for (int j=0; j < 508; j++) {
+    printf("%c", namey[j]);
+  }
+  printf("%c", m);
+  for (int w=0; w < 4; w++) {
+    printf("%c", ind[w]);
+  }
+
+	open_file(name, mode);
 }
 
 // close 'file'.  Always sets 'fserror' global.
@@ -96,14 +162,14 @@ unsigned long file_length(File file){
 // Fails if the file is currently open. Returns 1 on success, 0 on failure. 
 // Always sets 'fserror' global.   
 int delete_file(char *name){
-  // check dir for name - get block number
-  File file = lookup_file(name);
+	// check dir for name - get block number
+	File file = lookup_file(name);
 
 	// grab and reset dir bit vector for block number to 0
-  // dir_bit_vector[file->dir_block_index] = 0;
+	// dir_bit_vector[file->dir_block_index] = 0;
 
 	// reset inode bit vector for block grabbed to 0
-  // like above but for inode
+	// like above but for inode
 } 
 
 // determines if a file with 'name' exists and returns 1 if it exists, otherwise 0.
